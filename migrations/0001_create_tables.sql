c3e4c831305358f8089e5c2c844cc863f34d58b4
Aditya30ag <adityaagrwal3005@gmail.com>
2025-08-31 20:00:22 +0530

    new

---
+++ migrations/0001_create_tables.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "description" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
